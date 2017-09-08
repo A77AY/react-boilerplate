c0c503d908213c2983b7762cab411e16b0834da8
Rinat Arsaev <krickray@gmail.com>
2017-09-09 00:12:06 +0500

    run-up server.jsx for hmr

--- src/server/server.jsx
+++ src/server/server.jsx
@@ -6,6 +6,12 @@ const app = express();
 
 app.use((...args) => routes(...args));
 
+if (module.hot) {
+    module.hot.accept('./routes', () => {
+        System.import('./routes').then(routes => routes)
+    });
+}
+
 app.listen(App.Server.PORT, App.Server.HOST, function () {
     console.log(`App listening on port ${App.Server.PORT}`)
 });
